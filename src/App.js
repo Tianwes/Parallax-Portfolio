37fafc029709e670bcee5963c7ae811b1de5e1bf
Tianwes <willnotsuffice@gmail.com>
2021-10-12 10:54:10 -0400

    Parallax works, needs tweeks but skeleton there

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navigation from "./components/Navbar/Navbar";
 // import Landing from './components/Landing/Landing';
+// import tanLineLol from "./assets/tan-line-lol.png"
+import ParallaxJSXWrapper from './components/Parallax/ParallaxJSXWrapper';
+import Navigation from "./components/Navbar/Navbar";
 import Image from 'react-bootstrap/Image'
 import portfolioPic from "./assets/CoffeePortfolioPic.png";
-import tanLineLol from "./assets/tan-line-lol.png"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      {/* <Landing /> */}
 
-      <div className="section1">
+  const Section1 = ParallaxJSXWrapper(
+    <div className="section1">
     <h2 className="nameh2">Tiffany Weaver</h2>
     <h1 className="nameh1">Web Developer</h1>
     <Image className="bio-pic" src={ portfolioPic } fluid />
-    </div>
+    </div> , 0.04);
 
-      <div className="section2" >
+  const Section2 = ParallaxJSXWrapper(
+    <div className="section2" >
   <svg className="svg_curve_top" xmlns="http://www.w3.org/2000/svg" fill="rgb(68, 48, 15)" viewBox="0 0 100 100" preserveAspectRatio="none" >
     <path d="M0,100 C65,93 76,10 100,100" />
   </svg>
@@ -33,20 +32,62 @@ function App() {
     </div>
   </div>
   <svg className="svg_curve_bottom" xmlns="http://www.w3.org/2000/svg" fill="rgb(68, 48, 15)" viewBox="0 0 100 100" preserveAspectRatio="none" >
-  <path d="M0,0 C65,20 90,5 100,0" />
+  <path d="M0,100 C65,93 76,10 100,100" />
   </svg>
-</div>
-      <div className="section3">
+</div> , 0.05);
+const Section3 = ParallaxJSXWrapper(
+  <div className="section3">
         <p>loremipsum ipsum loremipsum loremipsum</p>
       </div>
-      <div className="section4">
+ , 0.02);
+ const Section4 = ParallaxJSXWrapper(
+<div className="section4">
         <h1>Contacts</h1>
         <p>phone</p>
         <p>email</p>
         <p>twitter</p>
         <p>linkedIn</p>
-      </div>
+      </div>);
 
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Landing /> */}
+<Section1 />
+      {/* <div className="section1">
+    <h2 className="nameh2">Tiffany Weaver</h2>
+    <h1 className="nameh1">Web Developer</h1>
+    <Image className="bio-pic" src={ portfolioPic } fluid />
+    </div> */}
+<Section2 />
+      {/* <div className="section2" >
+  <svg className="svg_curve_top" xmlns="http://www.w3.org/2000/svg" fill="rgb(68, 48, 15)" viewBox="0 0 100 100" preserveAspectRatio="none" >
+    <path d="M0,100 C65,93 76,10 100,100" />
+  </svg>
+  <div className="section2_container">
+    <div className="left_side">
+      loremipsumdkf sdfijfks nascuhef sdvjsdiufhwn efkjdsncdosenf loremipsumdkf sdfijfks nascuhef sdvjsdiufhwn efkjdsncdosenf
+    </div>
+    <div className="right_side">
+      <p>vnosidnvsdkvjewn</p>
+    </div>
+  </div>
+  <svg className="svg_curve_bottom" xmlns="http://www.w3.org/2000/svg" fill="rgb(68, 48, 15)" viewBox="0 0 100 100" preserveAspectRatio="none" >
+  <path d="M0,100 C65,93 76,10 100,100" />
+  </svg>
+</div> */}
+      {/* <div className="section3">
+        <p>loremipsum ipsum loremipsum loremipsum</p>
+      </div> */}
+   <Section3 />   
+      {/* <div className="section4">
+        <h1>Contacts</h1>
+        <p>phone</p>
+        <p>email</p>
+        <p>twitter</p>
+        <p>linkedIn</p>
+      </div> */}
+<Section4 />
     </div>
   );
 }
